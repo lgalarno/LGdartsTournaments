15b09aea3e750212bfa8762c22daffaa59c76bf1
Luc Galarneau <lgalarno@gmail.com>
2024-01-06 23:32:57 -0500

    Cosmetics: links in tables, bootstrap icons cdn

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Game, Participant
+from .models import Game, Participant, Tournament
 
 
 class ParticipantInline(admin.TabularInline):
@@ -14,3 +14,4 @@ class GameAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Game, GameAdmin)
 admin.site.register(Participant)
+admin.site.register(Tournament)
